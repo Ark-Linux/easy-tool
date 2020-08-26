b49b64e9a3161b0cad9fcb595fcf5b91ee71dcef
HokyGUAN <hoky.guan@tymphany.com>
2020-08-26 17:59:55 +0800

    fixed bug between linux and windows env

--- pyeasyTool/Module/base.py
+++ pyeasyTool/Module/base.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import platform
 from time import sleep
 
 class Base:
@@ -10,7 +11,7 @@ class Base:
             print("\t%d. %s"%(i+1,self.array[i]))
     def get_num(self):
         menu_num=input("Enter Number:")
-        if (len(menu_num)==0 or len(menu_num)>2):
+        if (len(menu_num) == 0):
             self.input=0
         else:
             if ((int(menu_num)-1) < len(self.array)):
@@ -18,13 +19,22 @@ class Base:
             else:
                 self.input=0
 
+def __env_check():
+    if platform.system() == 'Windows':
+        return True
+    else:
+        return False
+
 def get_device_num():
     command_info=os.popen('adb devices').read()
     device_num=command_info.split("\t")[0].split("\n")[1]
     return device_num
 
 def monitor():
-    subprocess.call('adb shell "adk-message-monitor -a"', creationflags=subprocess.CREATE_NEW_CONSOLE)
+    if __env_check():
+        subprocess.call('adb shell "adk-message-monitor -a"', creationflags=subprocess.CREATE_NEW_CONSOLE)
+    else:
+        os.popen('''gnome-terminal -x adb shell "adk-message-monitor -a"''')
 
 def is_exist():
     while True:
